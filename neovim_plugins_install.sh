8c2409c4bac0c194a9c05bbe2bd2ace7307a37da
Rory Coffey <coffeyrt@gmail.com>
2020-02-22 21:26:07 -0500

    Update neovim_plugins_install.sh

--- neovim_plugins_install.sh
+++ neovim_plugins_install.sh
@@ -1,24 +1,9 @@
 #! /usr/bin/bash
 
-cd ~
-mkdir .config
-cd .config
-mkdir nvim
-cd ~
-mkdir .local
-cd .local
-mkdir share
-cd share
-mkdir nvim
-cd nvim
-mkdir site
-cd site
-mkdir pack
-cd pack
-mkdir plugins
-cd plugins
-mkdir start
-cd start
+mkdir -p ~/.config/nvim/
+cp nvim_init.vim ~/.config/nvim/init.vim
+mkdir -p ~/.local/share/nvim/site/pack/plugins/start
+cd ~/.local/share/nvim/site/pack/plugins/start
 git clone https://github.com/ervandew/supertab.git
 git clone https://github.com/jiangmiao/auto-pairs.git
 git clone https://github.com/davidhalter/jedi-vim.git
@@ -27,8 +12,7 @@ git clone --recursive https://github.com/deoplete-plugins/deoplete-jedi
 git clone https://github.com/morhetz/gruvbox.git
 git clone https://github.com/honza/vim-snippets.git
 git clone https://github.com/SirVer/ultisnips.git
-cd ~
-cp ./vim_neovim_settings/nvim_init.vim ./.config/nvim/init.vim
+cd -
 pip install jedi
 pip install yapf
 pip install --user --upgrade pynvim
