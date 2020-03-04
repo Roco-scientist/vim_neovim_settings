4da5eb6340ab3d701228172891b014d4f69e7d4c
Rory Coffey <coffeyrt@gmail.com>
2020-03-03 19:28:47 -0500

    updates

--- vim_plugins_install.sh
+++ vim_plugins_install.sh
@@ -1,9 +1,8 @@
 #! /usr/bin/bash
 
-mkdir -p ~/.config/nvim/
-cp nvim_init.vim ~/.config/nvim/init.vim
-mkdir -p ~/.local/share/nvim/site/pack/plugins/start
-cd ~/.local/share/nvim/site/pack/plugins/start
+cp vimrc ~/.vimrc
+mkdir -p ~/.vim/pack/shapeshed/start/
+cd ~/.vim/pack/shapeshed/start/
 git clone https://github.com/ervandew/supertab.git
 git clone https://github.com/jiangmiao/auto-pairs.git
 git clone https://github.com/davidhalter/jedi-vim.git
@@ -18,4 +17,3 @@ cd -
 pip install jedi
 pip install yapf
 pip install --user --upgrade pynvim
-pip install --user neovim
