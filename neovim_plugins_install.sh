14d0729a67d7997380acb762c40524d4445565e2
Rory Coffey <coffeyrt@gmail.com>
2020-03-05 10:27:43 -0500

    Update neovim_plugins_install.sh

--- neovim_plugins_install.sh
+++ neovim_plugins_install.sh
@@ -8,6 +8,7 @@ git clone https://github.com/ervandew/supertab.git
 git clone https://github.com/jiangmiao/auto-pairs.git
 git clone https://github.com/Shougo/deoplete.nvim.git
 git clone --recursive https://github.com/deoplete-plugins/deoplete-jedi
+git clone https://github.com/davidhalter/jedi.git
 git clone https://github.com/morhetz/gruvbox.git
 git clone https://github.com/honza/vim-snippets.git
 git clone https://github.com/SirVer/ultisnips.git
