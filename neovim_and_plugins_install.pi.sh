84d7c41dcf0e984295c206e387015fc0d790b584
Rory Coffey <coffeyrt@gmail.com>
2021-10-22 07:28:11 -0400

    initial commit

---
+++ neovim_and_plugins_install.pi.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+ 
+VIM_DIR=$PWD
+
+sudo apt-get install ninja-build
+# install neovim
+cd
+sudo apt-get install libtool-bin
+git clone https://github.com/neovim/neovim
+cd neovim
+git checkout stable
+sudo make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+cd
+
+mkdir -p ~/.config/nvim/
+ln -s $VIM_DIR/init.lua ~/.config/nvim/init.lua
+ln -s $VIM_DIR/pep8 ~/.config/pep8
+ln -s $VIM_DIR/flake8 ~/.config/flake8
+cd -
+pip install jedi
+pip install yapf
+pip install --user flake8
+pip install pynvim
+pip install --user autopep8
+pip install neovim
+sudo apt-get install mypy
+cd
+
+# install packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# Install language servers
+# Python
+sudo npm i -g pyright
+
+# Lua
+git clone --depth=1 https://hub.fastgit.org/sumneko/lua-language-server
+cd lua-language-server
+git submodule update --init --recursive
+cd 3rd/luamake
+compile/install.sh
+cd ../..
+./3rd/luamake/luamake rebuild
+echo 'export PATH="$HOME/lua-language-server/bin/Linux:$PATH"' >> ~/.profile
+
+cd
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Rust
+git clone https://github.com/rust-analyzer/rust-analyzer && cd rust-analyzer
+cargo build --release
+
+# install rustfmt (for formatting)
+rustup component add rustfmt
+
+# install clippy (for semantic linting)
+rustup component add clippy
+
+# Install tree-sitter
+cargo install tree-sitter-cli
+
+sudo apt-get install ruby-dev
+# fd
+sudo apt install fd-find
+sudo npm install -g neovim
+sudo gem install neovim
+sudo apt install cpanminus
+sudo cpanm -n Neovim::Ext
+
+
+cd -
+echo "move rust-analyzer/target/release/rust-analyzer to /bin/"
+echo ":PackerSync"
+echo ":checkhealth"
+echo "Check that path in .profile is not after cargo/env"
