89a520ee2570ee134c93cd0f390f04bbdedce140
Rory Coffey <coffeyrt@gmail.com>
2021-11-04 17:48:27 -0400

    updated clangd

--- neovim_and_plugins_install.sh
+++ neovim_and_plugins_install.sh
@@ -32,7 +32,7 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 
 # Install language servers
 # C+ ---- update this later if needed
-sudo apt-get install clangd-12
+sudo apt-get install clangd
 
 # Python
 sudo npm i -g pyright
