fc9ed957e991dec9dd3d8f1be8ef9a3326b1ebba
Rory Coffey <coffeyrt@gmail.com>
2022-01-06 17:44:44 -0500

    more python2 info

--- neovim_and_plugins_install.sh
+++ neovim_and_plugins_install.sh
@@ -10,6 +10,7 @@ ln -s $PWD/flake8 ~/.config/flake8
 cd -
 # curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 # sudo python2 get-pip.py
+# python2 -m pip install neovim
 pip install jedi
 pip install yapf
 pip install black
