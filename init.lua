0cc62b5e05e795f0e2c9ed9b5e15b289a47d68db
Rory Coffey <coffeyrt@gmail.com>
2023-06-12 22:20:42 +0000

    removed deprecated

--- init.lua
+++ init.lua
@@ -379,7 +379,7 @@ require('nvim-treesitter.configs').setup {
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Enable the following language servers
 -- local servers = { 'rust_analyzer', 'pyright' }
@@ -443,38 +443,38 @@ require('rust-tools').setup(opts)
 -- vim.cmd('let R_assign = 0')
 
 -- Lua
-local sumneko_binary_path = vim.fn.exepath('lua-language-server')
-local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
+-- local sumneko_binary_path = vim.fn.exepath('lua-language-server')
+-- local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
 
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
+-- local runtime_path = vim.split(package.path, ';')
+-- table.insert(runtime_path, "lua/?.lua")
+-- table.insert(runtime_path, "lua/?/init.lua")
 
-require'lspconfig'.sumneko_lua.setup {
-    cmd = {sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua"};
-    settings = {
-        Lua = {
-        runtime = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-            -- Setup your lua path
-            path = runtime_path,
-        },
-        diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {'vim'},
-        },
-        workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-            enable = false,
-        },
-        },
-    },
-}
+-- require'lspconfig'.sumneko_lua.setup {
+--     cmd = {sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua"};
+--     settings = {
+--         Lua = {
+--         runtime = {
+--             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+--             version = 'LuaJIT',
+--             -- Setup your lua path
+--             path = runtime_path,
+--         },
+--         diagnostics = {
+--             -- Get the language server to recognize the `vim` global
+--             globals = {'vim'},
+--         },
+--         workspace = {
+--             -- Make the server aware of Neovim runtime files
+--             library = vim.api.nvim_get_runtime_file("", true),
+--         },
+--         -- Do not send telemetry data containing a randomized but unique identifier
+--         telemetry = {
+--             enable = false,
+--         },
+--         },
+--     },
+-- }
 
 -- Typescript ls
 local buf_map = function(bufnr, mode, lhs, rhs, opts)
